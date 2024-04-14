b290f3c192fe4cc16150d993d65e8a98ef9642fd
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-04-14 14:18:51 +0530

    hello

--- src/components/random.js
+++ src/components/random.js
@@ -4,6 +4,11 @@ import Navbar from "./navbar";
 import QuestionCard from "./questionCard";
 import QuestionSet from "./QuestionSet";
 import Filters from "./filter";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 
 const Random = () => {
@@ -11,7 +16,8 @@ const Random = () => {
     const [qLevel, setQLevel] = useState('');
     const [qTopic, setQTopic] = useState('');
     const [filteredQSet, setFilteredQSet] = useState([]);
-    const [noOfRandom, setNoOfRandom]=useState(10)
+    const [customInput, setCustomInput] = useState("");
+    const [noOfRandom, setNoOfRandom] = useState(10);
 
     function getRandomElements(arr, numElements) {
         const newArr = arr.slice();
@@ -24,9 +30,9 @@ const Random = () => {
         
         return randomElements;
     }
-    const randomQuestions=getRandomElements(QuestionSet, noOfRandom)
-
+    
     useEffect(() => {
+        const randomQuestions = getRandomElements(QuestionSet, noOfRandom)
         const filterFunction = (question) => {
             return (
                 (qTopic === '' || question.topic === qTopic) &&
@@ -36,20 +42,45 @@ const Random = () => {
         };
         const filteredQuestions = randomQuestions.filter(filterFunction);
         setFilteredQSet(filteredQuestions);
-    }, [qType, qLevel, qTopic]);
-    
+    }, [qType, qLevel, qTopic, noOfRandom]);
+
+    const handleNoOfQuestion = (number) => {
+        setNoOfRandom(number);
+    }
+
+    const handleCustomInput = (e) => {
+        setNoOfRandom(parseInt(e.target.value) || 0);
+        setCustomInput(e.target.value);
+    }
 
     return (
         <>
             <Navbar />
             <div className="" style={{ cursor: "pointer", margin: "4rem 2rem", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", width: "100vw" }}>
                 <Filters qType={setQType} qLevel={setQLevel} qTopic={setQTopic} />
+
+                <FormControl style={{ color: "white" }}>
+                    <FormLabel id="demo-row-radio-buttons-group-label" style={{ color: "white" }}>Random Questions</FormLabel>
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                    >
+                        <FormControlLabel value="10" control={<Radio />} label="10" onClick={() => handleNoOfQuestion(10)} />
+                        <FormControlLabel value="20" control={<Radio />} label="20" onClick={() => handleNoOfQuestion(20)} />
+                        <FormControlLabel value="30" control={<Radio />} label="30" onClick={() => handleNoOfQuestion(30)} />
+                        <FormControlLabel
+                            value="custom"
+                            control={<Radio />}
+                            label={<input type="text" value={customInput} style={{ backgroundColor: "white", borderRadius: "2rem", color: "black", padding: "0.2rem" }} onChange={handleCustomInput} />}
+                        />
+                    </RadioGroup>
+                </FormControl>
                 
                 <QuestionCard questions={filteredQSet} />
             </div>
         </>
     )
 }
-
 
 export default Random;
